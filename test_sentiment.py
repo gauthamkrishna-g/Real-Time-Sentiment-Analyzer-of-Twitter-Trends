f0757f53dc031e392bb1989a22f07d7f8b111430
G. Gautham Krishna <gauthkris@gmail.com>
2017-06-07 18:06:18 +0530

    Sentiment of user-given Text

--- test_sentiment.py
+++ test_sentiment.py
@@ -6,9 +6,25 @@ Created on Thu Feb  2 10:21:32 2017
 """
 
 import custom_sentimentanalyzer as sent
+from textblob import TextBlob
+
 text = input("Enter Text: ")
 
-category, confidence = sent.sentiment(text)
 print ("Text: ", text)
+print ()
+print ("Custom Sentiment: ")
+category, confidence = sent.sentiment(text)
+print ("Category: ", category)
+print ("Confidence: {:0.2f}" .format(confidence))
+
+print ()
+
+print ("TextBlob Sentiment: ")
+blob = TextBlob(text)
+confidence = blob.sentiment.polarity
+if confidence > 0:
+    category = "pos"
+else:
+    category = "neg"
 print ("Category: ", category)
 print ("Confidence: {:0.2f}" .format(confidence))
